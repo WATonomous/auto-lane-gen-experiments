faaed0e2652e2ad5cd757cd6334b592e7d18185c
jmogen <jmogen@uwaterloo.ca>
2023-05-09 02:18:05 +0000

    pushed so brian can take a look

--- src/atrous_attn_vit/vit-imagenet.py
+++ src/atrous_attn_vit/vit-imagenet.py
@@ -295,6 +295,10 @@ model = ViT(
     mlp_dim = 2048,
     dropout = 0.1
 )
+if torch.cuda.device_count() > 1:
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+    model = nn.DataParallel(model)
 model.to(device)
 losses = []
 # Random parameters chosen - similar to LaNet parameters but with nesterov
@@ -312,6 +316,13 @@ scheduler = StepLR(optimizer, step_size=80, gamma=gamma)
 #         nesterov=True
 #     )
 #scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, float(epochs))
+
+for data in trainloader:
+    input = data.to(device)
+    output = model(input)
+    print("Outside: input size", input.size(),
+          "output_size", output.size())
+
 
 for e in range(epochs):
     print(f"\nepoch {e+1}/{epochs}")
