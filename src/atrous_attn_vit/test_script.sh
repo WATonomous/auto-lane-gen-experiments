4fd3dfd799784548db5e437c65c3d77a830a1ff4
jmogen <jmogen@uwaterloo.ca>
2023-05-29 15:47:38 +0000

    script update

--- src/atrous_attn_vit/test_script.sh
+++ src/atrous_attn_vit/test_script.sh
@@ -1,5 +1,5 @@
-# python vit-imagenet.py --seed 55 --epoch 300 --patchsize 112 --atrousAttn 2>&1 |& tee vit_atrous1.txt
-# python vit-imagenet.py --seed 55 --epoch 300 --patchsize 56 --atrousAttn 2>&1 |& tee vit_atrous2.txt
+python vit-imagenet.py --seed 55 --epoch 300 --patchsize 112 --atrousAttn 2>&1 |& tee vit_atrous1.txt
+python vit-imagenet.py --seed 55 --epoch 300 --patchsize 56 --atrousAttn 2>&1 |& tee vit_atrous2.txt
 python vit-imagenet.py --seed 55 --epoch 300 --patchsize 32 --atrousAttn 2>&1 |& tee vit_atrous3.txt
 # python vit-imagenet.py --seed 55 --epoch 30 --patchsize 32  2>&1 |& tee vit_reg2.txt
 # python vit-imagenet.py --seed 55 --epoch 30 --patchsize 32 --atrousAttn 2>&1 |& tee vit_atrous2.txt
